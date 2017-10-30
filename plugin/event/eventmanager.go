57ed6ad8a386fc2e0fcb394722b13f2ec5257788
olsdavis <olsdavis@gmail.com>
2017-10-30 15:34:32 +0100

    :construction: Small plugin commit.

--- plugin/event/eventmanager.go
+++ plugin/event/eventmanager.go
@@ -1,5 +1,11 @@
 package event
 
+type EventHook interface {
+	// OnEvent is a function called whenever the event for which
+	// this EventHook has been registered is called.
+	OnEvent(event *Event)
+}
+
 type EventManager struct {
 
 }
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,1 +1,8 @@
 package plugin
+
+// Plugin struct represents an extension for goelan which can handle events,
+// create new commands, etc. In short: customize the server thanks to an API
+// for the used language.
+type Plugin struct {
+	Language Language
+}
